31675d52a49254d56859ffe851d566f7943b5e2d
crosshairz <crosshair@centrum.sk>
2015-03-01 09:59:15 +0100

    Update p3.config.sample.js

--- p3.config.sample.js
+++ p3.config.sample.js
@@ -14,3 +14,45 @@ module.exports = {
         }
     }
 };
+{
+    "css": {
+        "font": [
+        ],
+        "import": [
+        ],
+        "rule": {
+            "#chat .message:nth-child(2n+1),#chat .mention:nth-child(2n+1),#chat .skip:nth-child(2n+1),#chat .moderation:nth-child(2n+1),#chat .emote:nth-child(2n+1),#chat .update:nth-child(2n+1),#chat .welcome:nth-child(2n+1)": {
+                "background-color": "rgba(17, 19, 23, 0.75)"
+            },
+            "#chat .emote.from-4573622": {
+                "border-left": "#ac76ff 3px solid",
+                "background-color": "rgba(24, 0, 38, 0.9) !important"
+            
+            },
+            ".emote.from-4573622 .from.staff.clickable": {
+                "display": "none"
+            },
+            ".emote.from-4573622 .icon-chat-manager": {
+                "display": "none"
+            },
+            "#chat .emote.from-4573622 .from": {
+                 "color": "#ac76ff !important"
+            },
+            "#chat .emote.from-4573622 .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            }
+        }
+    },
+    "images": {
+        "background": "http://i.imgur.com/c1iLKz6.png",
+        "booth": "",
+        "playback": "http://i.imgur.com/8xFgQmU.png"
+    },
+    "rules": {
+        "allowAutorespond": "false",
+        "allowAutowoot": "true",
+	    "allowAutojoin": "false"
+    }
+}
